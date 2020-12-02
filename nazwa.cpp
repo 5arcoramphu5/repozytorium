f71d937dcb1ac7b5c2f20e411ade2a07f3ac9d82
5arcoramphu5 <74541183+5arcoramphu5@users.noreply.github.com>
2020-12-02 12:07:35 +0100

    Update nazwa.cpp

--- nazwa.cpp
+++ nazwa.cpp
@@ -1,10 +1,11 @@
 #include <random>
+#include <iostream>
 using namespace std;
 int main()
 {
     random_device device;
     default_random_engine e(device());
     uniform_int_distribution<int> uniform_dist(1, 6);
-    return uniform_dist(e);
-    //?
+    for(int i = 0; i<10; i++) cout<<uniform_dist(e)<<" ";
+    return 0;
 }
